753a4b6d383334c47bcad1bea1a1571bfbae7bea
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2020-03-16 18:07:48 +0100

    link settings panel

--- app/src/main/java/org/noise_planet/qrtoneapp/ChatActivity.kt
+++ app/src/main/java/org/noise_planet/qrtoneapp/ChatActivity.kt
@@ -2,11 +2,14 @@ package org.noise_planet.qrtoneapp
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -30,6 +33,7 @@ class ChatActivity : AppCompatActivity(), PropertyChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        //setSupportActionBar(toolbar)
 
         messageList.layoutManager = LinearLayoutManager(this)
         adapter = MessageAdapter(this)
@@ -49,6 +53,26 @@ class ChatActivity : AppCompatActivity(), PropertyChangeListener {
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this@ChatActivity, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun initAudioProcess() {
         listening.set(true)
         var audioProcess = AudioProcess(listening)
